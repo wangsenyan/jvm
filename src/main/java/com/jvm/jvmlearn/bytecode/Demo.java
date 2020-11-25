487abc86acc5949bc0fda773dce32ce82bec9632
wangsenyan <2633600702@qq.com>
2020-11-26 01:04:37 +0800

    方法

--- src/main/java/com/jvm/jvmlearn/bytecode/Demo.java
+++ src/main/java/com/jvm/jvmlearn/bytecode/Demo.java
@@ -1,9 +1,9 @@
 package com.jvm.jvmlearn.bytecode;
 
 public class Demo {
-    private final int num = 1;
+    private  int num = 1;
     public int add(){
-        //num = num +2;
+        num = num +2;
         return num;
     }
 }
